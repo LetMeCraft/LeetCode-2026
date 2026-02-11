96cd618055707143e9c1724d4145520cc9523b06
LetMeCraft <236047309+LetMeCraft@users.noreply.github.com>
2026-02-11 23:49:11 +0530

    Solve: 2293. Problem 2293 (Medium)

---
+++ Medium/2293_problem_2293_23909b.py
@@ -0,0 +1,14 @@
+# 2293. LeetCode Problem
+# LeetCode Link: https://leetcode.com/problems/
+
+class Solution:
+    def solve(self):
+        """
+        LeetCode Problem #2293
+        
+        AI generation temporarily unavailable.
+        Visit LeetCode to solve this problem manually.
+        """
+        pass
+
+# Solved: 2026-02-11
